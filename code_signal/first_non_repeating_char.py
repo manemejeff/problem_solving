d4f7ddd763fcfe9277e7056110f5059ae797beec
manemejeff <kmb14056@yandex.ru>
2021-05-19 16:34:46 +0400

    added codesignal added first_duplicate.py added first_non_repeating_char.py

---
+++ code_signal/first_non_repeating_char.py
@@ -0,0 +1,48 @@
+'''
+Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+Example
+
+    For s = "abacabad", the output should be
+    firstNotRepeatingCharacter(s) = 'c'.
+
+    There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+    For s = "abacabaabacaba", the output should be
+    firstNotRepeatingCharacter(s) = '_'.
+
+    There are no characters in this string that do not repeat.
+
+Input/Output
+
+    [execution time limit] 4 seconds (py3)
+
+    [input] string s
+
+    A string that contains only lowercase English letters.
+
+    Guaranteed constraints:
+    1 ≤ s.length ≤ 105.
+
+    [output] char
+
+    The first non-repeating character in s, or '_' if there are no characters that do not repeat.
+
+'''
+
+
+def firstNotRepeatingCharacter(s):
+    # seen = {char: s.count(char) for char in s}
+    seen = {}
+    for char in s:
+        if char not in seen:
+            seen[char] = 1
+        else:
+            seen[char] += 1
+
+    for char in s:
+        if seen[char] == 1:
+            return char
+    return '_'
+
+
