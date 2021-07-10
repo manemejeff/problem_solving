eb37bda63d03396738c5719ac0095cce0a3cf631
manemejeff <kmb14056@yandex.ru>
2021-07-10 20:27:18 +0400

    open the lock no solution yet

---
+++ python_deep_dive/Part1/1.QuickRefresher/garbage_collection.py
@@ -0,0 +1,29 @@
+import ctypes
+import gc
+
+
+def ref_count(address):
+    return ctypes.c_long.from_address(address).value
+
+
+def object_by_id(object_id):
+    for obj in gc.get_objects():
+        if id(obj) == object_id:
+            return 'Object exists'
+    return 'Not found'
+
+
+class A:
+    def __init__(self):
+        self.b = B(self)
+        print('A: self: {0}, b: {1}'.format(hex(id(self)), hex(id(self.b))))
+
+
+class B:
+    def __init__(self, a):
+        self.a = a
+        print('B: self: {0}, a: {1}'.format(hex(id(self)), hex(id(self.a))))
+
+if __name__ == '__main__':
+
+    my_var = A()
