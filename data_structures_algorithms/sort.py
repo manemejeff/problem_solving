6e1c5992e145c19e3f327c2f2e973902819a044d
manemejeff <kmb14056@yandex.ru>
2021-11-15 06:09:39 +0400

    add data structs and algorithms recursion.py, sortings(bubble, selection, insertion, merge)

---
+++ data_structures_algorithms/sort.py
@@ -0,0 +1,70 @@
+def bubble_sort(my_list: list) -> list:
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+    return my_list
+
+
+def selection_sort(my_list: list) -> list:
+    for i in range(len(my_list) - 1):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
+    return my_list
+
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+
+def merge(list1: list, list2: list) -> list:
+    combined = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+
+def merge_sort(my_list: list) -> list:
+    """
+    creating new lists
+    O(n) - space complexity
+
+    O(n log n) - time complexity
+    :param my_list:
+    :return:
+    """
+    if len(my_list) == 1:
+        return my_list
+    mid = int(len(my_list) / 2)
+    left = my_list[:mid]
+    right = my_list[mid:]
+    return merge(merge_sort(left), merge_sort(right))
+
+
+if __name__ == '__main__':
+    unsorted_list = [5, 16, 1, 12, 2, 3, 8]
+    print(merge_sort(unsorted_list))
